d8311180a4f02de905f7c5add31b575defdacd81
spencermuparutsa-create <spencer.muparutsa@gmail.com>
2026-03-29 09:53:46 +0200

    Add files via upload

--- space.py
+++ space.py
@@ -15,6 +15,8 @@ ship1r = pygame.transform.rotate(ship1s,90)
 ship2 = pygame.image.load("lesson 4 - space invaders/images/spaceship_yellow.png")
 ship2s = pygame.transform.scale(ship2,(90,80))
 ship2r = pygame.transform.rotate(ship2s,270)
+shoot = pygame.mixer.Sound("lesson 4 - space invaders/images/Gun+Silencer.mp3")
+collide = pygame.mixer.Sound("lesson 4 - space invaders/images/Grenade+1.mp3")
 
 RHealth = 10
 YHealth = 10
@@ -50,11 +52,23 @@ def yellowMove():
     
 clock = pygame.time.Clock()
 
+redbullets = []
+yellowbullets = []
+
 while True:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LSHIFT:
+                bullet = pygame.Rect(red.x+90,red.y+40,10,7)
+                shoot.play()
+                redbullets.append(bullet)
+            if event.key == pygame.K_RSHIFT:
+                bullets = pygame.Rect(yellow.x,red.y+40,10,7)
+                shoot.play()
+                yellowbullets.append(bullets)
     screen.blit(space,(0,0))
     keys_pressed = pygame.key.get_pressed()
     redMove()
@@ -67,4 +81,15 @@ while True:
     textY = font.render("Health:" + str(YHealth),True,"white")
     screen.blit(textR,(10,10))
     screen.blit(textY,(1260,10))
+    for rb in redbullets:
+        pygame.draw.rect(screen,"red",bullet)
+        rb.x+=9
+        if yellow.colliderect(rb):
+            collide.play()
+            redbullets.remove(rb)
+            YHealth -= 1
+    for yb in yellowbullets:
+        pygame.draw.rect(screen,"yellow",bullets)
+        yb.x-=9
+
     pygame.display.update()
